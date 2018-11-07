3037bb9d11029987f3b6c20af0b627e674b13521
Alan Lo <lo.alan@gmail.com>
2018-11-08 10:03:52 +1100

    refactor with stuff from home laptop. added bashhistory, made chcolor use localvars, bash_profile will auto source ~/.bash_local if exists

--- osx/README.md
+++ osx/README.md
@@ -1,3 +1,12 @@
 # OSX bash stuff
 
-...
+# bash_profile
+Just source [bash_profile](bash_profile) for stuff
+
+
+# chcolor.sh
+A script for changing the terminal themes based on names of currently installed themes in OSX terminal. Check the script for details or run
+```bash
+chcolor.sh -h
+```
+You'll need to have it in PATH for it to be automatically run every time a new terminal starts for a random theme.
--- osx/chcolor.sh
+++ osx/chcolor.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 ### Changes the terminal theme.
 
-
 # Install themes from https://github.com/lysyi3m/macos-terminal-themes first!
-profiles=("3024 Day" "3024 Night" "AdventureTime" "Material" "SeaShells" "Tomorrow Night Blue")
-
+# Set TERMINAL_THEMES as a comma separated list in environment to have local perferences
+# Otherwise the default list are two from the default OSX themes
+if [ ${#TERMINAL_THEMES[@]} -eq 0 ]; then
+    TERMINAL_ARRAY=("Novel" "Man Page")
+else
+    IFS=',' read -r -a TERMINAL_ARRAY <<< "$TERMINAL_THEMES"
+fi
 
 if [ -z "$1" ]; then
 	RANDOM=$$$(date +%s)
-	pick=${profiles[$RANDOM % ${#profiles[@]}]}
+	pick=${TERMINAL_ARRAY[$RANDOM % ${#TERMINAL_ARRAY[@]}]}
 elif [ "$1" == "-h" ]; then
     echo "Usage:"
     echo "   chcolor.sh (name of theme)"
-    echo "Name of theme is optional. If omitted, it selects randomly from list of 'profiles' array."
+    echo "Changes the current theme of an OSX terminal."
+    echo "Name of theme is optional. If omitted, it selects randomly from list of 'TERMINAL_THEMES' array."
     echo ""
-	echo "profiles=${profiles[@]}"
+	echo "Current TERMINAL_THEMES=\""$(IFS=,;printf "%s" "${TERMINAL_ARRAY[*]}")"\""
+    echo ""
+    echo "TERMINAL_THEMES is a comma list of theme names because bash doesn't export arrays well"
+    echo ""
 	exit
 else
 	pick=$1
